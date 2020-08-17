9427eabb9ce7c5662f2059a73f23bb5afb7be5fe
NarothamSai <narotham@buildstorm.com>
2020-08-17 19:04:56 +0530

    Add kubernetes scripts and logs

---
+++ delete_kubernetes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# This file deletes the kubernetes cluster
+
+kubectl delete deployment --force house-prediction
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=narothamsai/house-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment house-prediction --image=$dockerpath 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
+kubectl rollout status deployment house-prediction
+
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/house-prediction 8000:80
